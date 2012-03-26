dbefc09761d19e4d8068ced5e99cb9d423e29a4f
unknown <Tarde@LabFibra-Pc13.(none)>
2012-03-26 20:20:23 +0200

    This should work

--- ex1.py
+++ ex1.py
@@ -5,5 +5,5 @@ print "Hello Again"
 print "I like typing this."
 print "This is fun."
 print 'Yay! Printing.'
-print "I'd much rather you 'not'."
-print 'I "said" do not touch this.'
+print "I'd much rather you 'not'"
+print 'I "said" do not touch this'
