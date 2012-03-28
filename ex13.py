168d8dc69179d86b1abe3202b89ecc7fbb9d41a1
Alex B <paraipanakos@gmail.com>
2012-03-28 19:38:15 +0200

    Coding away...

--- ex13.py
+++ ex13.py
@@ -1,2 +1,13 @@
 
+from sys import argv
 
+script, x, y, z = argv
+
+print "The script is called:", script
+print "Your first variable is:", x
+print "Your second variable is:", y
+print "Your third variable is:", z
+
+print x,y,z
+
+stuff = raw_input("do you like %s, %s or %s the most ?" %(x,y,z))
