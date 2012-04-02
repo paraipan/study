5a74053c435d1388730b362cf55e7782523f368f
Alex B <paraipanakos@gmail.com>
2012-04-03 00:25:34 +0200

    Coding away...

--- ex19.py
+++ ex19.py
@@ -4,12 +4,12 @@ def cheese_and_crackers(cheese_count, boxes_of_crackers):
     print "You have %d boxes of crackers!" % boxes_of_crackers
     print "Man that's enough for a party!"
 
-
 def bottles_and_glasses(bottles_count, glasses_count):
     print "Then we need %d bottles." % bottles_count
     print "And 4 times more than %d glasses." %glasses_count
     print "Then we can really party !"
     print "Get a blanket.\n"
+
 
 print "We can just give the function numbers directly:"
 cheese_and_crackers(20, 30)
