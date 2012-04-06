8fdc9dc75ec0d501d405f5157e1a8a681d5a16f0
Alex B <paraipanakos@gmail.com>
2012-04-06 23:24:51 +0200

    Coding away...

--- ex25.py
+++ ex25.py
@@ -29,8 +29,8 @@ def print_first_and_last(sentence):
     print_first_word(words)
     print_last_word(words)
 
-def print_first_and_last(sentence):
-    """Sorts the word then prints the first and last one."""
+def print_first_and_last_sorted(sentence):
+    """Sorts the words then prints the first and last one."""
     words = sort_sentence(sentence)
     print_first_word(words)
     print_last_word(words)
