bff572fdb444ef9dfd053334ab38e6502a7fc867
Alex B <paraipanakos@gmail.com>
2012-04-10 17:48:10 +0200

    Coding away...

---
+++ ex30.py
@@ -0,0 +1,28 @@
+
+people = 41
+cars = 40
+buses = 80
+
+
+if cars > people:
+    print "We should take the cars."
+elif cars < people:
+    print "We should not take the cars."
+else:
+    print "We can't decide."
+
+if buses > cars:
+    print "That's too many buses."
+elif buses < cars:
+    print "Maybe we could take the buses."
+else:
+    print "We still can't decide."
+
+if people > buses:
+    print "Alright, let's just take the buses."
+elif people < buses:
+    print "Are you sure there are so many buses ? Please count them again:\n"
+    buses = raw_input("> ")
+else:
+    print "Fine, let's stay home then."
+
