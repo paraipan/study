3a485f39ec163485da1bf5faf1b11845d6057682
Alex B <paraipanakos@gmail.com>
2012-04-11 17:58:36 +0200

    Coding away...

--- ex33.py
+++ ex33.py
@@ -7,3 +7,19 @@ while i < 6:
     numbers.append(i)
 
     i = i + 1
+    print "Numbers now: ", numbers
+    print "At the bottom i is %d" % i
+
+print "The numbers: "
+
+for num in numbers:
+    print num
+
+def loop(sum):
+    while sum < 6:
+        print "At the top i is %d" % sum
+        numbers.append(sum)
+
+        sum = sum + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % sum
